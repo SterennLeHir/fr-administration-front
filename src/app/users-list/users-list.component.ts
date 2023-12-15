61756e433b21126f7db3aec9ad85a6cc852ee1a8
Sterenn <slh35@hotmail.fr>
2023-12-15 09:44:07 +0100

    Update table

--- src/app/users-list/users-list.component.ts
+++ src/app/users-list/users-list.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MatTableModule} from "@angular/material/table";
+import {CommonModule} from "@angular/common";
+
+export class User {
+  constructor(
+    public id: number,
+    public password: string,
+    public lastname: string,
+    public firstname: string,
+    public age: number
+  ) {}
+}
+const users: User[] = [
+  new User(0, 'mdp1', 'Doe', 'John', 23),
+  new User(1, 'mdp2', 'Doe', 'Jane', 32)
+]
 
 @Component({
   selector: 'app-users-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatTableModule],
   templateUrl: './users-list.component.html',
   styleUrl: './users-list.component.css'
 })
-export class UsersListComponent {
+export class UsersListComponent implements OnInit {
+  ngOnInit(): void {
+  }
+  displayedColumns: string[] = ['id', 'lastname', 'firstname', 'age'];
+  dataSource = users;
 
 }
