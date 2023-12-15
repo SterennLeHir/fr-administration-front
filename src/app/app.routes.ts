415137ab65cf24a4b430bee2cf53c2c1ec9340be
Sterenn <slh35@hotmail.fr>
2023-12-15 09:44:39 +0100

    Create login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {UsersListComponent} from "./users-list/users-list.component";
+import {LoginComponent} from "./login/login.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'users', component: UsersListComponent },
+  { path: '', component: LoginComponent }
+];
