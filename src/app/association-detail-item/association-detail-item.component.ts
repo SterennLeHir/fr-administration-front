8e57050551e6a7ac872e46581b79d00cfb071d4b
Sterenn <slh35@hotmail.fr>
2024-01-02 22:07:56 +0100

    Fix roles association

--- src/app/association-detail-item/association-detail-item.component.ts
+++ src/app/association-detail-item/association-detail-item.component.ts
@@ -39,25 +39,48 @@ export class AssociationDetailItemComponent implements OnInit{
   ngOnInit(): void {
     this.route.paramMap
       .subscribe(res => {
-        const id = res.get("id");
-        if (id != null) {
-          const userRequest: Observable<any> = this.http.get('http://localhost:3000/associations/'+id, { observe: 'response' });
+        this.id = res.get("id");
+        if (this.id != null) {
+          const userRequest: Observable<any> = this.http.get('http://localhost:3000/associations/'+this.id, { observe: 'response' });
           lastValueFrom(userRequest).then(response => this.association = response.body);
           /*this.api.get({endpoint : '/associations/'+id}).then(response => {
             console.log(response);
             this.association = response;
           });*/
-          this.api.get({endpoint : '/associations/' + id + '/members'}).then(response => {
+          this.api.get({endpoint : '/associations/' + this.id + '/members'}).then(response => {
             this.members = response;
+            this.nbMembers = this.members.length;
+            for (const member of this.members) {
+              this.api.get({endpoint : '/roles/' + member.id + '/' + this.id}).then(response => {
+                console.log(response.name);
+                this.roles.set(member, response.name);
+              });
+            }
           });
-          this.api.get({endpoint : '/minutes/association/' + id}).then(response => {
+          this.api.get({endpoint : '/minutes/association/' + this.id}).then(response => {
             this.minutes = response;
+            this.nbMinutes = this.minutes.length;
           });
         }
       })
   }
 
+  getRole(member:User) {
+    return this.roles.get(member);
+  }
   association!:Association;
   members: User[] = [];
   minutes: Minute[] = [];
+  nbMinutes !: number;
+  nbMembers !: number;
+  id !: string | null;
+  roles= new Map();
+}
+
+export class Role {
+  id !: number;
+  association !: Association;
+  user !: User;
+  name !: string;
+
 }
