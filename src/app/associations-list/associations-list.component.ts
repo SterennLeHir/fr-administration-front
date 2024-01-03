28798ee29664452154444c4fe9c2b0f6bbace435
Sterenn <slh35@hotmail.fr>
2024-01-03 12:28:47 +0100

    Add search association

--- src/app/associations-list/associations-list.component.ts
+++ src/app/associations-list/associations-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NavComponent} from "../nav/nav.component";
 import {HttpClient} from "@angular/common/http";
 import {lastValueFrom, Observable} from "rxjs";
@@ -8,6 +8,7 @@ import {NgForOf} from "@angular/common";
 import {UserItemComponent} from "../user-item/user-item.component";
 import {AssociationItemComponent} from "../association-item/association-item.component";
 import {RouterLink, RouterLinkActive} from "@angular/router";
+import {ApiHelperService} from "../services/api-helper.service";
 
 @Component({
   selector: 'app-associations-list',
@@ -25,16 +26,32 @@ import {RouterLink, RouterLinkActive} from "@angular/router";
   templateUrl: './associations-list.component.html',
   styleUrl: './associations-list.component.css'
 })
-export class AssociationsListComponent {
+export class AssociationsListComponent implements OnInit {
 
   constructor(
     private http: HttpClient
   ) {}
 
+
+  dataSource: any= [];
   ngOnInit(): void {
-    const request: Observable<any> = this.http.get('http://localhost:3000/associations', { observe: 'response' });
+    const request: Observable<any> = this.http.get('http://localhost:3000/associations', {observe: 'response'});
     console.log(lastValueFrom(request));
     lastValueFrom(request).then(response => this.dataSource = response.body);
   }
-  dataSource= [];
+
+  search(e: KeyboardEvent) {
+    const searchTerm = (e.target as HTMLInputElement).value;
+    if (searchTerm === "") {
+      const request: Observable<any> = this.http.get('http://localhost:3000/associations/', {observe: 'response'});
+      lastValueFrom(request).then(response => this.dataSource = response.body);
+    } else if (e.key === 'Enter') {
+      // La touche Entrée a été enfoncée
+      // Récupérer la valeur de la barre de recherche
+      const searchTerm = (e.target as HTMLInputElement).value;
+      this.dataSource = [];
+      const request: Observable<any> = this.http.get('http://localhost:3000/associations/' + searchTerm, {observe: 'response'});
+      lastValueFrom(request).then(response => this.dataSource.push(response.body));
+      }
+    }
 }
