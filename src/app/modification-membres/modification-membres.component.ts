f940e1d2e582d4da849aad55251bd84e87bc508d
Sterenn <slh35@hotmail.fr>
2024-01-03 23:13:13 +0100

    Fix role update

--- src/app/modification-membres/modification-membres.component.ts
+++ src/app/modification-membres/modification-membres.component.ts
@@ -32,7 +32,8 @@ export class ModificationMembresComponent implements OnInit {
   constructor(private router: Router, private api: ApiHelperService, private http: HttpClient, private route: ActivatedRoute) {}
 
   private assocId!:number;
-  public members !: User[]; // membres
+  public members !: User[]; // membres initaux
+  public membersModified : User[] = []// membres au cours de la modification
   public users !: User[];
   public selectedUser !:User;
   public selectedRole !: RoleValue;
@@ -44,29 +45,33 @@ export class ModificationMembresComponent implements OnInit {
         const id = res.get("id");
         if (id != null) {
           this.assocId = +id;
-          this.api.get({endpoint : '/associations/' + id + '/members'}).then(response => {
-            this.members = response;
-            for (const member of this.members) {
-              this.api.get({endpoint: '/roles/' + member.id + '/' + id}).then( response=> {
-                // attribution du role du membre
-                member.role = response.name;
+          this.api.get({endpoint: '/associations/' + id + '/members'}).then(response => {
+            this.members = response; // tableau initial qui ne changera pas durant la modification
+            // Promise.all pour attendre la fin de toutes les requêtes asynchrones
+            Promise.all(this.members.map(member => this.api.get({endpoint: '/roles/' + member.id + '/' + id})))
+              .then(responses => {
+                // responses est un tableau contenant les réponses pour chaque membre
+                for (let i = 0; i < this.members.length; i++) {
+                  // attribution du rôle à chaque membre
+                  this.members[i].role = responses[i].name;
+                }
+                // copie du tableau de membres avec les rôles attribués
+                this.membersModified = this.members.map(member => ({...member}));
               })
-            }
-          }).then(response => {
-            const userRequest: Observable<any> = this.http.get('http://localhost:3000/users', { observe: 'response' });
-            lastValueFrom(userRequest).then(response => this.users = response.body);
-          }).then(response=>{
-            console.log("MEMBERS" + JSON.stringify(this.members));
+              .then(response => {
+                const userRequest: Observable<any> = this.http.get('http://localhost:3000/users', {observe: 'response'});
+                lastValueFrom(userRequest).then(response => this.users = response.body);
+              })
           })
         }
       })
-    }
+  }
   validate(): void {
     console.log('ON VALIDE');
     // On met à jour les membres
     this.api.put({ endpoint: '/associations/'+ this.assocId,
       data: { idUsers: this.membersToId()}}).then(response => {
-      this.router.navigateByUrl('/associations/'+this.assocId);
+      this.router.navigateByUrl('/associations/'+ this.assocId);
     })
     // On met à jour les rôles
     this.modifyRoles();
@@ -74,7 +79,7 @@ export class ModificationMembresComponent implements OnInit {
 
   modifyRoles() {
     // mise à jour des rôles pour tous les anciens membres
-    for (const member of this.members) {
+    for (const member of this.membersModified) {
       this.api.put({ endpoint: '/roles/'+ member.id + '/' + this.assocId,
         data: { name: member.role}}).then(response => {
         console.log(`role modifié pour l'utilisateur ${member.id} dans l'association ${this.assocId}`);
@@ -95,27 +100,55 @@ export class ModificationMembresComponent implements OnInit {
   }
 
   deleteMember(user:User): void{
-    const i = this.members.indexOf(user);
-    this.membersToDeleteId.push(this.members[i].id); // On ajoute l'utilisateur dans la liste de ceux à supprimer de l'association
-    this.members.splice(i, 1);
+    console.log("début méthode delete");
+    console.log(`Membres au début de deleteMember : ${JSON.stringify(this.members)}`);
+    const i = this.members.indexOf(user); // position du user dans la liste initiale
+    console.log(i);
+    console.log(this.membersToDeleteId);
+    if (i !== -1) { // on l'ajoute aux membres à supprimer que s'il appartenait à la liste initiale
+      this.membersToDeleteId.push(this.members[i].id);
+      console.log(`Membres après l'ajout dans ceux à supprimer : ${JSON.stringify(this.members)}`);
+    }
+    // on le supprime de la liste modifiée ou de la liste des nouveaux users
+    const i2 = this.newMembers.findIndex(member => user.id === member.id); // position du user dans la liste modifiée
+    if (i2 !== -1) { // si c'est un nouveau membre
+      this.newMembers.splice(i2, 1);
+      console.log(`Membres après la suppression dans les nouveaux membres : ${JSON.stringify(this.members)}`);
+    } else {
+      const i3 = this.membersModified.findIndex(member => user.id === member.id); // position du user dans la liste modifiée
+      this.membersModified.splice(i3, 1);
+      console.log(`Membres après la suppression dans les membres modifiés: ${JSON.stringify(this.members)}`);
+    }
     console.log("Deleting member:", user);
-    console.log("MEMBERS" + JSON.stringify(this.members));
+    console.log(`Membres à la fin de deleteMember : ${JSON.stringify(this.members)}`);
   }
 
   addMember(): void{
-    const idPosition = this.members.findIndex(member => this.selectedUser.id === member.id);
-    if((idPosition === -1)){ //on ne add que si le user selected n'est pas déjà membre
+    const idInModifiedList= this.membersModified.concat(this.newMembers).findIndex(member => this.selectedUser.id === member.id);
+    if(idInModifiedList === -1){ // on n'ajoute le userSelected que s'il n'est pas déjà membre (liste modifiée)
       this.selectedUser.role = RoleValue.M; // Valeur par défaut
       if (this.membersToDeleteId.indexOf(this.selectedUser.id) !== -1) {// l'utilisateur a été supprimé sans valider
-        console.log("C'est un faux nouveau membre");
-        this.members.push(this.selectedUser);
+        console.log("Il a déjà été supprimé");
         // On l'enlève des utilisateurs à supprimer
         const indexToDelete = this.membersToDeleteId.indexOf(this.selectedUser.id);
         this.membersToDeleteId.splice(indexToDelete, 1);
-      } else { // on ajoute un vrai nouveau membre
-        this.newMembers.push(this.selectedUser);
-        console.log('Adding member:', this.selectedUser);
+        console.log("enlevé de la liste à supprimer", this.membersToDeleteId)
       }
+      // on regarde si c'est un nouveau membre ou un présent dans la liste initiale
+      const idInInitialList= this.members.findIndex(member => this.selectedUser.id === member.id);
+      console.log(`indice dans liste initiale ${idInInitialList}`);
+      console.log(`indice dans liste modifiée ${idInModifiedList}`);
+      if(idInInitialList !== -1) { // si c'est un ancien membre on modifie son role
+        console.log("Ce n'est pas un nouveau membre");
+        console.log(`L'ancienne liste était ${JSON.stringify(this.membersModified)}`);
+        this.membersModified.push(this.selectedUser);
+        console.log(`La nouvelle liste est ${JSON.stringify(this.membersModified)}`);
+      } else { // si c'est un nouveau membre on l'ajoute à la liste des nouveaux membres
+        this.newMembers.push(this.selectedUser);
+      }
+
+      console.log('Adding member:', this.selectedUser);
+
     }
   }
   private membersToId(): number[]{
