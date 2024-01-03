d7fc12634e422660bbd27e0a2c10b73e058f7402
Sterenn <slh35@hotmail.fr>
2024-01-03 10:22:09 +0100

    Fix Minute

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,6 +12,7 @@ import {AssociationDetailItemComponent, Minute} from "./association-detail-item/
 import {ModificationAssociationComponent} from "./modification-association/modification-association.component";
 import {ModificationPasswordComponent} from "./modification-password/modification-password.component";
 import {CreationAssociationComponent} from "./creation-association/creation-association.component";
+import {MinuteComponent} from "./minute/minute.component";
 
 export const routes: Routes = [
   {path: 'users', component: UsersListComponent, canActivate:[authGuard]},
@@ -22,7 +23,7 @@ export const routes: Routes = [
   {path: 'account', component:AccountComponent, canActivate:[authGuard]},
   {path: 'users/:id', component:UserDetailItemComponent, canActivate:[authGuard]},
   {path: 'associations/:id', component:AssociationDetailItemComponent, canActivate:[authGuard]},
-  {path: 'minute/:id', component: Minute, canActivate: [authGuard]},
+  {path: 'minute/:id', component: MinuteComponent, canActivate: [authGuard]},
   {path: 'modifier-mes-informations', component:ModificationInfosComponent, canActivate:[authGuard]},
   {path: 'modifier-association/:id', component:ModificationAssociationComponent, canActivate:[authGuard]},
   {path: 'modifier-mot-de-passe', component:ModificationPasswordComponent, canActivate:[authGuard]},
