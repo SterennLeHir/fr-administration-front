6fb1e2c8fa4b81dff7cb72f17adde6061a967fdc
adenlexar <96285929+adenlexar@users.noreply.github.com>
2024-01-04 00:02:49 +0100

    Update creation-association

--- src/app/creation-association/creation-association.component.ts
+++ src/app/creation-association/creation-association.component.ts
@@ -28,11 +28,7 @@ import {lastValueFrom, Observable} from "rxjs";
   templateUrl: './creation-association.component.html',
   styleUrl: './creation-association.component.css'
 })
-export class CreationAssociationComponent implements OnInit{
-  private assocId!:number;
-  public members! : User[]
-  public users! : User[]
-  public selectedUser! :User
+export class CreationAssociationComponent{
   constructor(
     private http: HttpClient,
     private route: ActivatedRoute,
@@ -41,42 +37,14 @@ export class CreationAssociationComponent implements OnInit{
     private token: TokenStorageService
   ) {}
 
-  ngOnInit(): void {
-    const userRequest: Observable<any> = this.http.get('http://localhost:3000/users', { observe: 'response' });
-    lastValueFrom(userRequest).then(response => this.users = response.body);
-    this.members = []
-  }
-
   validate(): void {
     console.log('ON CREE')
     const nom: string = (document.getElementById('nom') as HTMLInputElement).value;
     const description: string = (document.getElementById('description') as HTMLInputElement).value;
-    const membersId: number[] = this.membersToId()
     this.api.post({ endpoint: '/associations',
-      data: { name: nom, idUsers: membersId, description: description}}).then(response => {
+      data: { name: nom, idUsers: [], description: description}}).then(response => {
       this.router.navigateByUrl('/associations');
     })
   };
-
-  deleteMember(user:User): void{
-    const i = this.members.indexOf(user)
-    this.members.splice(i, 1)
-    console.log("Deleting member:", user)
-    console.log("MEMBERS" + JSON.stringify(this.members))
-  }
-
-  addMember(): void{
-    if((this.members.findIndex(member => this.selectedUser.id === member.id) === -1)){ //on ne add que si le user selected n'est pas déjà membre
-      this.members.push(this.selectedUser)
-      console.log('Adding member:', this.selectedUser);
-      console.log("MEMBERS" + JSON.stringify(this.members))
-    }
-  }
-
-  private membersToId(): number[]{
-    return this.members.map(member => member.id)
-  }
-
   protected readonly JSON = JSON;
-
 }
